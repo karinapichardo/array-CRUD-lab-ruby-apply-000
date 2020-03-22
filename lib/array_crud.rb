b411144665b46b5983a2cadf204107a1f796c438
Karina Pichardo <pichardokarina21@yahoo.com>
2020-03-22 02:33:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,25 @@
 def create_an_empty_array
   
+  [ ]
+  
 end
 
 def create_an_array
   
+ [1, 2, 3, 4]
+ 
 end
 
 def add_element_to_end_of_array(array, element)
   
+  [ ].push("arrays!")
+  
 end
 
 def add_element_to_start_of_array(array, element)
+  first_array = [ ]
+  
+  first_array.shift("arrays!")
   
 end
 
