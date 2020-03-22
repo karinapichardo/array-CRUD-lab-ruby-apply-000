6d3591953fd55221bc69c671e8652b9a74832cb7
Karina Pichardo <pichardokarina21@yahoo.com>
2020-03-22 02:53:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,8 +24,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  array_end = [ ]
   
-  [ ].pop("arrays!")
+  array_end.pop("arrays!")
   
 end
 
